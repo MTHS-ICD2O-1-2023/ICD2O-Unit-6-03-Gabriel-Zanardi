5071108330e246184132b0693a57f89febe2b3e6
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-22 18:11:11 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+      '<img src="' +
+      jsonData.url +
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+        "<p>Artist: " +
+        '<a href="' +
+        jsonData.artist_url +
+        '">' +
+        jsonData.artist +
+        "</a>"
+    } else {
+      document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getImage("https://api.catboys.com/img")
