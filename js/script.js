75e8cc0cdac6ea33489ed28a912301d3b05c86cd
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-22 18:55:44 +0000

    Fixed js 3/4

--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@
 
 "use strict"
 
-function myButtonClicked() {
-
-  const getImage = async("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5") => {
+  async function myButtonClicked() {
     try {
-      const result = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-      const jsonData = await result.json()
-      console.log(jsonData)
+      const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
+      const jsonData = await resultJSON.json()
+      //console.log(jsonData)
+      const tempOttawa = jsonData.main.temp
+      console.log(tempOttawa)
+      // convert to C
+      // then show
+      document.getElementById("final-answer").innerHTML = "The temperature in Ottawa is " + tempOttawa
+    } catch (error) {
+      console.error(error)
     }
-
-}
+  }
